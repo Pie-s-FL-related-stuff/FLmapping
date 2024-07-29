1c0c2676620f5a712eb8c4a74b8a496180c66ae3
Pietagorh <pietagorhh@proton.me>
2024-07-30 00:13:00 +0200

    remove sorting code

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 import json
-import shutil
-from os import path
 from PIL import Image
-
-
-def load(name: str):
-    with open("maps.json", "r") as file:
-        map_data = json.load(file)[name]
-    for i, column in enumerate(map_data):
-        for j, tile in enumerate(column):
-            if tile == "" or path.exists(f"map_parts/tex1_512x512_{tile}_3.png"):
-                continue
-            shutil.move(f"unfiltered/tex1_512x512_{tile}_3.png", f"map_parts/tex1_512x512_{tile}_3.png")
 
 
 def compile_map(name: str):
@@ -30,6 +18,4 @@ def compile_map(name: str):
 
 
 if __name__ == "__main__":
-    a = "ancient_ruins"
-    load(a)
-    compile_map(a)
+    compile_map("ancient_ruins")
