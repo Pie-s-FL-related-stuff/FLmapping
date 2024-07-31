6656f4b49f5733b6cd370600cf453dc4f19ddced
Pietagorh <pietagorhh@proton.me>
2024-07-31 12:33:54 +0200

    moved util functions to a different file

---
+++ compile_maps.py
@@ -0,0 +1,27 @@
+"""
+Turns map tiles (as found in the game) into full maps.
+"""
+from PIL import Image
+from util import get_map_data, get_top_left_corner, get_down_right_corner, get_tile
+
+MAP = "deep_elderwood"
+
+
+def compile_map(map_name: str):
+    map_data = get_map_data()[map_name]
+
+    map_image = Image.new("RGB", size=(len(map_data[0]) * 512, len(map_data) * 512), color=(0, 255, 0))
+
+    for i, column in enumerate(map_data):
+        for j, tile in enumerate(column):
+            map_image.paste(get_tile(map_name, j, i, tile), (j * 512, i * 512))
+
+    return map_image
+
+
+def remove_borders(image: Image.Image) -> Image.Image:
+    return image.crop((*get_top_left_corner(image), *get_down_right_corner(image)))
+
+
+if __name__ == "__main__":
+    remove_borders(compile_map(MAP)).show()
---
+++ util.py
@@ -0,0 +1,37 @@
+import json
+from PIL import Image
+
+
+def get_map_data() -> dict[str, list[list[str]]]:
+    with open("maps.json", "r") as file:
+        map_data = json.load(file)
+    return map_data
+
+
+def get_tile(map_name: str, x: int, y: int, tile: str) -> Image.Image:
+    if tile != "":
+        return Image.open(f"map_parts/tex1_512x512_{tile}_3_mip0.png")
+    try:
+        return Image.open(f"missing_parts/{map_name}/{y + 1}_{x + 1}.png")
+    except FileNotFoundError:
+        print(f"Missing tile at {(x + 1, y + 1)} in {map_name} (might mess with automatic cropping)")
+        return Image.new("RGB", size=(512, 512), color=(0, 255, 0))
+
+
+def get_top_left_corner(image: Image.Image) -> tuple[int, int]:
+    pixel_map = image.convert("RGB").load()
+    for i in range(image.width * image.height):
+        x, y = divmod(i, image.height)
+        print(x, y, pixel_map[x, y])
+        if pixel_map[x, y] != (255, 0, 255):
+            return x, y
+    return 0, 0
+
+
+def get_down_right_corner(image: Image.Image) -> tuple[int, int]:
+    pixel_map = image.load()
+    for i in range(image.width * image.height - 1, 0, -1):
+        x, y = divmod(i, image.height)
+        if pixel_map[x, y] != (255, 0, 255):
+            return x + 1, y + 1
+    return image.width, image.height
