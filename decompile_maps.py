dd6665925ad0bbace6f3d91bd55b16f270ab4fad
Pietagorh <pietagorhh@proton.me>
2024-07-31 12:34:12 +0200

    decompile maps

---
+++ decompile_maps.py
@@ -0,0 +1,34 @@
+"""
+Turns full maps back into map tiles that work for Citra texture packs.
+"""
+from PIL import Image
+from util import get_map_data, get_tile, get_top_left_corner
+
+
+def add_pink_borders(map_name: str, upscale_factor: float) -> Image.Image:
+    map_data = get_map_data()[map_name]
+
+    size = (int(512 * len(map_data[0]) * upscale_factor), int(512 * len(map_data) * upscale_factor))
+    image = Image.new("RGB", size=size, color=(255, 0, 255))
+
+    image.paste(Image.open(f"edited_maps/{map_name}.png"), get_top_left_corner(get_tile(map_name, 0, 0, map_data[0][0])))
+
+    return image
+
+
+def decompile_map(map_name: str, upscale_factor: float):
+    map_data = get_map_data()[map_name]
+
+    edited_map = Image.open(f"edited_maps/{map_name}.png")
+
+    for i in range(len(map_data)):
+        for j in range(len(map_data[0])):
+            tile_name = map_data[i][j]
+            if tile_name == "":
+                continue
+            edited_tile = edited_map.crop((j * 512 * upscale_factor, i * 512 * upscale_factor, (j + 1) * 512 * upscale_factor, (i + 1) * 512 * upscale_factor))
+            edited_tile.save(f"edited_tiles/tex1_512x512_{tile_name}_3.png")
+
+
+if __name__ == "__main__":
+    decompile_map("east_castele", 4)
